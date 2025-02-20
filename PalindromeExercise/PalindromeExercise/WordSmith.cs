9fbd3269c855647a23cf8b59f1114d0513db16dc
colinjones26 <colinanthonyjones1996@gmail.com>
2025-02-20 09:45:07 -0500

    Initial Commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            if (string.IsNullOrEmpty(word))
+                return false;
+
+            int left = 0;
+            int right = word.Length - 1;
+
+            while (left < right)
+            {
+                if (word[left] != word[right])
+                {
+                    return false;
+                }
+
+                left++;
+                right--;
+            }
+            return true;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,9 +6,24 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        [InlineData("madam", true)]
+        [InlineData("apple",  false)]
+        [InlineData("a", true)]
+        [InlineData("", false)]
+
+        public void TestIsAPalindrome(string input, bool expected)
         {
+            //Arrange
+            WordSmith wordSmith = new WordSmith();
+
+            //Act
+            bool actual = wordSmith.IsAPalindrome(input);
+
+            //Assert
+            Assert.Equal(expected, actual);
 
         }
     }
